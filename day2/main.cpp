b008e62c22fcd31c3b28ed2681426f6ebee15a9f
Emiel van Dam <edam@slb.com>
2023-12-05 10:08:11 +0100

    day2 1/2 works

--- day2/main.cpp
+++ day2/main.cpp
@@ -4,28 +4,42 @@
 #include <regex>
 #include <unordered_map>
 
-std::unordered_map<std::string, int> game = {
+std::unordered_map<std::string, int> constraints = {
     // 12 red cubes, 13 green cubes, and 14 blue cubes.
     {"red", 12},
     {"green", 13},
     {"blue", 14}};
 
-int main(int, char **) {
+int main(int, char **)
+{
   std::ifstream ifs("./input.txt");
   std::string line;
+  int sum = 0;
 
-  while (std::getline(ifs, line)) {
-
+  while (std::getline(ifs, line))
+  {
+    bool didPass = true;
     auto game = split(line, ":");
-    auto turns = split(game[1], ";");
-    for (auto turn : turns) {
-      auto actions = split(turn, ",");
-      for (auto action : actions) {
+    for (auto turn : split(game[1], ";"))
+    {
+      for (auto action : split(turn, ","))
+      {
         auto kv = split(ltrim(action), " ");
-
-        std::cout << "key=" << kv[1] << std::endl;
+        auto key = kv[1];
         auto value = std::stoi(kv[0]);
+        if (value > constraints[key])
+        {
+          didPass = false;
+          break;
+        }
       }
     }
+    if (didPass)
+    {
+      auto id = std::stoi(ltrim(split(game[0], " ")[1]));
+      sum += id;
+    }
   }
+  std::cout << sum << std::endl;
+  return 0;
 }
